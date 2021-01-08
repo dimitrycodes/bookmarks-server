cd2d4bd05059606c4c50ace813188e65a2b32c3f
dimitrycodes <strongsafetyking@yahoo.com>
2021-01-08 17:15:27 +0600

    feat: get route by single id

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const store = require('./store');
+const logger = require('./logger');
 
 const bookmarksRouter = express.Router();
 
@@ -7,4 +8,17 @@ bookmarksRouter.route('/bookmarks').get((req, res) => {
   res.json(store.bookmarks);
 });
 
-module.exports = bookmarksRouter
+bookmarksRouter.route('/bookmarks/:bookmark_id').get((req, res) => {
+  const { bookmark_id } = req.params;
+
+  const bookmark = store.bookmarks.find((c) => c.id == bookmark_id);
+
+  if (!bookmark) {
+    logger.error(`Bookmark with id ${bookmark_id} not found.`);
+    return res.status(404).send('Bookmark Not Found');
+  }
+
+  res.json(bookmark);
+});
+
+module.exports = bookmarksRouter;
